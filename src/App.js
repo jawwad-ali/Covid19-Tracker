27c390020862f371f5df04c4282ec58cda1272ef
jawwad-ali <33836051+jawwad-ali@users.noreply.github.com>
2020-07-10 00:19:50 +0500

    Update App

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { fetchData } from './api'
 import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 
 import {Global , Chart , CountryPicker} from './Components'
